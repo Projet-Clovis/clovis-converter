85cb22fa748cbd91293856ec809827a8293e9abf
Ab2nour <briochekine@hotmail.fr>
2022-04-08 14:08:26 +0200

    it seems to work well :)

--- src/clovis_to_clovis.py
+++ src/clovis_to_clovis.py
@@ -6,6 +6,8 @@ And later, may be used to validate study sheet?
 from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 
+REMOVE_ENDING_BR_TAGS = ('h1', 'h2', 'h3', 'h4', 'p')
+REMOVE_EMPTY_TAGS = ('b', 'i')
 TAGS_LIST = ('h1', 'h2', 'h3', 'h4', 'p', 'b', 'i')
 COLORFUL_BLOCKS = ('definition', 'excerpt', 'quote', 'example', 'byheart',
                 'danger', 'summary', 'reminder', 'advice', 'remark')
@@ -37,21 +39,25 @@ class MyHTMLParser(HTMLParser):
                 self.doc += '<p class="definition-title">'
                 self.stack.append('p')
 
-            elif 'h1' in attrs['class']:
+            elif attrs['class'] == 'title':
                 self.doc += '<h1 class="title">'
                 self.stack.append('h1')
 
-            elif 'h2' in attrs['class']:
+            elif 'subtitle' in attrs['class']:
                 self.doc += '<h2 class="title">'
                 self.stack.append('h2')
 
-            elif 'h3' in attrs['class']:
+            elif 'subpart' in attrs['class']:
                 self.doc += '<h3 class="title">'
                 self.stack.append('h3')
 
-            elif 'h4' in attrs['class']:
+            elif 'subhead' in attrs['class']:
                 self.doc += '<h4 class="title">'
                 self.stack.append('h4')
+
+            elif 'text' in attrs['class']:
+                self.doc += '<p class="text">'
+                self.stack.append('p')
 
             else:
                 self.stack.append('p')
@@ -95,9 +101,9 @@ class MyHTMLParser(HTMLParser):
             self.doc += f'</{matching_tag}>\n'
 
         elif tag == 'b':
-            self.doc += '</b>\n'
+            self.doc += '</b>'
         elif tag == 'i':
-            self.doc += '</i>\n'
+            self.doc += '</i>'
 
         elif tag == 'section':
             self.doc += '</div>\n'
@@ -114,118 +120,10 @@ class MyHTMLParser(HTMLParser):
 parser = MyHTMLParser()
 
 
-study_sheet_example = '''<div class="preview" id="main-content" style="padding-left: 25px;">
- <div class="container toggle-h1" data-hide="h1-1">
-  <p class="title" contenteditable="false" data-count="I - " placeholder="Titre">
-   Some h1
-  </p>
-  <div class="toggle-title-container">
-  </div>
- </div>
- <div class="container toggle-h2 hide-h1-1" data-hide="h2-1">
-  <p class="subtitle" contenteditable="false" data-count="A) " placeholder="Sous-titre">
-   Some h2
-  </p>
-  <div class="toggle-title-container">
-  </div>
- </div>
- <div class="container toggle-h3 hide-h1-1 hide-h2-1" data-hide="h3-1">
-  <p class="subpart" contenteditable="false" data-count="a) " placeholder="Sous-partie">
-   Some h3
-  </p>
-  <div class="toggle-title-container">
-  </div>
- </div>
- <div class="container toggle-h4 hide-h1-1 hide-h2-1 hide-h3-1" data-hide="h4-1">
-  <p class="subhead" contenteditable="false" data-count="1) " placeholder="Titre inférieur">
-   Some h4
-  </p>
-  <div class="toggle-title-container">
-  </div>
- </div>
- <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1">
-  <p class="text" contenteditable="false" placeholder="Entrez du texte">
-   Some text
-   <br/>
-  </p>
- </div>
- <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1">
-  <p class="text" contenteditable="false" placeholder="Entrez du texte">
-   Some
-   <span class="hl-yellow">
-    highlighted text
-   </span>
-   , some
-   <b>
-    bold text
-   </b>
-   , some
-   <i>
-    italic text
-   </i>
-   , some
-   <i>
-    <b>
-     bold and italic
-    </b>
-   </i>
-   , some
-   <span class="hl-yellow">
-    <b>
-     bold and highlighted
-    </b>
-   </span>
-   , some
-   <span class="hl-yellow">
-    <i>
-     <b>
-      bold, italic highlighted text
-     </b>
-    </i>
-   </span>
-   <i>
-    <b>
-    </b>
-   </i>
-   .
-   <br/>
-  </p>
- </div>
- <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1">
-  <p class="text" contenteditable="false" placeholder="Entrez du texte">
-   Some
-   <span class="f-code">
-    inline code
-   </span>
-   .
-   <br/>
-  </p>
- </div>
- <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1">
-  <section class="colorful-block danger">
-   <article class="cb-content">
-    <p contenteditable="false" placeholder="Avertissement important">
-     Some warning
-     <br/>
-    </p>
-   </article>
-  </section>
- </div>
- <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1">
-  <section class="colorful-block definition">
-   <article class="cb-content">
-    <p class="definition-title" contenteditable="false" placeholder="Mot défini">
-     Some word
-     <br/>
-    </p>
-    <p contenteditable="false" placeholder="Définition">
-     Some definition
-     <br/>
-    </p>
-   </article>
-  </section>
- </div>
-</div>'''
+study_sheet_example = '''<div id="main-content" class="preview" style="padding-left: 25px;">
+
+
+            <div class="container toggle-h1" data-hide="h1-1"><p placeholder="Titre" class="title" data-count="I - " contenteditable="false">Some h1</p><div class="toggle-title-container"><i class="material-icons toggle-title"></i></div></div><div class="container toggle-h2 hide-h1-1" data-hide="h2-1" style=""><p placeholder="Sous-titre" class="subtitle" data-count="A) " contenteditable="false">Some h2</p><div class="toggle-title-container"><i class="material-icons toggle-title"></i></div></div><div class="container toggle-h3 hide-h1-1 hide-h2-1" data-hide="h3-1" style=""><p placeholder="Sous-partie" class="subpart" data-count="a) " contenteditable="false">Some h3</p><div class="toggle-title-container"><i class="material-icons toggle-title"></i></div></div><div class="container toggle-h4 hide-h1-1 hide-h2-1 hide-h3-1" data-hide="h4-1" style=""><p placeholder="Titre inférieur" class="subhead" data-count="1) " contenteditable="false">Some h4</p><div class="toggle-title-container"><i class="material-icons toggle-title"></i></div></div><div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1" style=""><p placeholder="Entrez du texte" class="text" contenteditable="false">Some text<br></p></div><div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1" style=""><p placeholder="Entrez du texte" class="text" contenteditable="false">Some <span class="hl-yellow">highlighted text</span>, some <b>bold text</b>, some <i>italic text</i>, some <i><b>bold and italic</b></i>, some <span class="hl-yellow"><b>bold and highlighted</b></span>, some <span class="hl-yellow"><i><b>bold, italic highlighted text</b></i></span><i><b></b></i>.<br></p></div><div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1" style=""><p placeholder="Entrez du texte" class="text" contenteditable="false">Some <span class="f-code">inline code</span>.<br></p></div><div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1" style=""><section class="colorful-block danger"><section class="cb-content"><article class="mini-title mt-danger">Attention</article><p placeholder="Avertissement important" contenteditable="false">Some warning<br></p></section></section></div>        <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1" style=""><section class="colorful-block definition"><section class="cb-content"><article class="mini-title mt-definition">Définition</article><p placeholder="Mot défini" class="definition-title" contenteditable="false">Some word<br></p><p placeholder="Définition" contenteditable="false">Some definition<br></p></section></section></div>                </div>'''
 
 
 ## Functions
@@ -254,5 +152,14 @@ rename_tags(soup, 'cb-content')
 
 parser.feed(str(soup))
 parser.doc += '\n'
+
+for tag in REMOVE_ENDING_BR_TAGS:
+    parser.doc = parser.doc.replace(f'<br></{tag}>', f'</{tag}>')
+
+# remove empty span, b, i, ...
+for i in range(5): # 5 times so it can replace nested empty tags
+    for tag in REMOVE_EMPTY_TAGS:
+        parser.doc = parser.doc.replace(f'<{tag}></{tag}>', '')
+
 print(parser.doc)
 
