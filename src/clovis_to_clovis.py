b93f6e3b99f2ea0febbad5c993a73efe57d9bf58
Ab2nour <briochekine@hotmail.fr>
2022-04-08 13:39:24 +0200

    Update clovis_to_clovis.py

--- src/clovis_to_clovis.py
+++ src/clovis_to_clovis.py
@@ -16,6 +16,7 @@ class MyHTMLParser(HTMLParser):
     def __init__(self):
         super().__init__()
         self.doc = ''
+        self.stack = []
 
     def handle_starttag(self, tag, attrs):
         print("Encountered a start tag:", tag, attrs)
@@ -30,10 +31,26 @@ class MyHTMLParser(HTMLParser):
         elif tag == 'h4':
             self.doc += '<h4 class="title">'
 
-        elif tag == 'p' and 'definition-title' in attrs['class']:
-            self.doc += '<p class="definition-title">'
+        # p with class
+        elif tag == 'p' and 'class' in attrs:
+            if 'definition-title' in attrs['class']:
+                self.doc += '<p class="definition-title">'
+
+            elif 'h1' in attrs['class']:
+                self.doc += '<h1 class="title">'
+
+            elif 'h2' in attrs['class']:
+                self.doc += '<h2 class="title">'
+
+            elif 'h3' in attrs['class']:
+                self.doc += '<h3 class="title">'
+
+            elif 'h4' in attrs['class']:
+                self.doc += '<h4 class="title">'
+
         elif tag == 'p':
             self.doc += '<p class="text">'
+            self.stack.append('text')
 
         elif tag == 'b':
             self.doc += '<b>'
@@ -86,20 +103,143 @@ class MyHTMLParser(HTMLParser):
 parser = MyHTMLParser()
 
 
-study_sheet_example = '''<div class="container"><div class="block-edit-button-container"></div><p placeholder="Titre" class="title" data-count="I - " contenteditable="false">Some h1</p></div><div class="container"><div class="block-edit-button-container"></div><p placeholder="Sous-titre" class="subtitle" data-count="A) " contenteditable="false">Some h2</p></div><div class="container"><div class="block-edit-button-container"></div><p placeholder="Sous-partie" class="subpart" data-count="a) " contenteditable="false">Some h3</p></div><div class="container"><div class="block-edit-button-container"></div><p placeholder="Titre inférieur" class="subhead" data-count="1) " contenteditable="false">Some h4</p></div><div class="container"><div class="block-edit-button-container"></div><p placeholder="Entrez du texte" class="text" contenteditable="false">Some text<br></p></div><div class="container"><div class="block-edit-button-container"></div><section class="colorful-block danger"><section class="cb-content"><article class="mini-title mt-danger">Attention</article><p placeholder="Avertissement important" contenteditable="false">Some warning<br></p></section></section></div>'''
+study_sheet_example = '''<div class="preview" id="main-content" style="padding-left: 25px;">
+ <div class="container toggle-h1" data-hide="h1-1">
+  <p class="title" contenteditable="false" data-count="I - " placeholder="Titre">
+   Some h1
+  </p>
+  <div class="toggle-title-container">
+  </div>
+ </div>
+ <div class="container toggle-h2 hide-h1-1" data-hide="h2-1">
+  <p class="subtitle" contenteditable="false" data-count="A) " placeholder="Sous-titre">
+   Some h2
+  </p>
+  <div class="toggle-title-container">
+  </div>
+ </div>
+ <div class="container toggle-h3 hide-h1-1 hide-h2-1" data-hide="h3-1">
+  <p class="subpart" contenteditable="false" data-count="a) " placeholder="Sous-partie">
+   Some h3
+  </p>
+  <div class="toggle-title-container">
+  </div>
+ </div>
+ <div class="container toggle-h4 hide-h1-1 hide-h2-1 hide-h3-1" data-hide="h4-1">
+  <p class="subhead" contenteditable="false" data-count="1) " placeholder="Titre inférieur">
+   Some h4
+  </p>
+  <div class="toggle-title-container">
+  </div>
+ </div>
+ <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1">
+  <p class="text" contenteditable="false" placeholder="Entrez du texte">
+   Some text
+   <br/>
+  </p>
+ </div>
+ <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1">
+  <p class="text" contenteditable="false" placeholder="Entrez du texte">
+   Some
+   <span class="hl-yellow">
+    highlighted text
+   </span>
+   , some
+   <b>
+    bold text
+   </b>
+   , some
+   <i>
+    italic text
+   </i>
+   , some
+   <i>
+    <b>
+     bold and italic
+    </b>
+   </i>
+   , some
+   <span class="hl-yellow">
+    <b>
+     bold and highlighted
+    </b>
+   </span>
+   , some
+   <span class="hl-yellow">
+    <i>
+     <b>
+      bold, italic highlighted text
+     </b>
+    </i>
+   </span>
+   <i>
+    <b>
+    </b>
+   </i>
+   .
+   <br/>
+  </p>
+ </div>
+ <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1">
+  <p class="text" contenteditable="false" placeholder="Entrez du texte">
+   Some
+   <span class="f-code">
+    inline code
+   </span>
+   .
+   <br/>
+  </p>
+ </div>
+ <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1">
+  <section class="colorful-block danger">
+   <article class="cb-content">
+    <p contenteditable="false" placeholder="Avertissement important">
+     Some warning
+     <br/>
+    </p>
+   </article>
+  </section>
+ </div>
+ <div class="container hide-h1-1 hide-h2-1 hide-h3-1 hide-h4-1">
+  <section class="colorful-block definition">
+   <article class="cb-content">
+    <p class="definition-title" contenteditable="false" placeholder="Mot défini">
+     Some word
+     <br/>
+    </p>
+    <p contenteditable="false" placeholder="Définition">
+     Some definition
+     <br/>
+    </p>
+   </article>
+  </section>
+ </div>
+</div>'''
 
 
+## Functions
+def remove_tags(soup, tags_class):
+    tags = soup.find_all(class_=tags_class)
+
+    for t in tags:
+        t.decompose() # remove
+
+
+def rename_tags(soup, tags_class, new_name='article'):
+    tags = soup.find_all(class_=tags_class)
+
+    for t in tags:
+        t.name = new_name
+
+
+## Main
 soup = BeautifulSoup(study_sheet_example, 'html.parser')
-mini_titles = soup.find_all(class_='mini-title')
 
-for a in mini_titles: # colorful-block titles
-    a.clear()
+remove_tags(soup, 'mini-title')
+remove_tags(soup, 'block-edit-button-container')
+remove_tags(soup, 'material-icons')
 
-cb_content = soup.find_all(class_='cb-content')
-
-for a in cb_content:
-    a.name = 'article'
-
+rename_tags(soup, 'cb-content')
 
 parser.feed(str(soup))
 parser.doc += '\n'
