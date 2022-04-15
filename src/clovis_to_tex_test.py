ecdebb7b3cb12916c59d96d5f5b043626958921e
Ab2nour <briochekine@hotmail.fr>
2022-04-15 23:33:44 +0200

    Update clovis_to_tex_test.py

--- src/clovis_to_tex_test.py
+++ src/clovis_to_tex_test.py
@@ -1,4 +1,4 @@
-# test h1
+# test for h1
 test_input = '<h1 class="title">Some h1 title</h1>'
 test_output = '\\section{Some h1 title}\n\n'
 result = clovis_to_tex(test_input)
@@ -6,7 +6,7 @@ result = clovis_to_tex(test_input)
 assert result == test_output
 
 
-# test definition
+# test for definition
 test_input = '''<div class="cb-container definition">
     <div class="cb-title-container">
         <span class="cb-title-icon"></span>
@@ -14,6 +14,20 @@ test_input = '''<div class="cb-container definition">
     </div>
     <p class="definition-title">Some word</p>
     <p class="text">Some definition</p>
+</div>'''
+test_output = '\\clovisDefinition{Some word}{\n    Some definition\n}\n\n'
+result = clovis_to_tex(test_input)
+
+assert result == test_output
+
+
+# test for colorful-block danger
+test_input = '''<div class="cb-container danger">
+    <div class="cb-title-container">
+        <span class="cb-title-icon"></span>
+        <span class="cb-title"></span>
+    </div>
+    <p class="text">Some warning</p>
 </div>'''
 test_output = '\\clovisDefinition{Some word}{\n    Some definition\n}\n\n'
 result = clovis_to_tex(test_input)
