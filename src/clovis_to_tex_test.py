2e6df2a0d492f7902c009058ea2d2a03e9bb4f07
Ab2nour <briochekine@hotmail.fr>
2022-04-16 11:50:15 +0200

    Update clovis_to_tex_test.py

--- src/clovis_to_tex_test.py
+++ src/clovis_to_tex_test.py
@@ -1,9 +1,9 @@
 # test for h1
 test_input = '<h1 class="title">Some h1 title</h1>'
-test_output = '\\section{Some h1 title}\n\n'
+expected_output = '\\section{Some h1 title}\n\n'
 result = clovis_to_tex(test_input)
 
-assert result == test_output
+assert result == expected_output
 
 
 # test for definition
@@ -15,10 +15,10 @@ test_input = '''<div class="cb-container definition">
     <p class="definition-title">Some word</p>
     <p class="text">Some definition</p>
 </div>'''
-test_output = '\\clovisDefinition{Some word}{\n    Some definition\n}\n\n'
+expected_output = '\\clovisDefinition{Some word}{\n    Some definition\n}\n\n'
 result = clovis_to_tex(test_input)
 
-assert result == test_output
+assert result == expected_output
 
 
 # test for colorful-block danger
@@ -29,7 +29,7 @@ test_input = '''<div class="cb-container danger">
     </div>
     <p class="text">Some warning</p>
 </div>'''
-test_output = '\\clovisDefinition{Some word}{\n    Some definition\n}\n\n'
+expected_output = '\\clovisDefinition{Some word}{\n    Some definition\n}\n\n'
 result = clovis_to_tex(test_input)
 
-assert result == test_output
+assert result == expected_output
