0f61150e348c97c961c0629fa0e7f71910958af2
Ab2nour <briochekine@hotmail.fr>
2022-04-25 16:52:00 +0200

    Update clovis_to_tex_test.py

--- src/clovis_to_tex_test.py
+++ src/clovis_to_tex_test.py
@@ -29,7 +29,23 @@ test_input = '''<div class="cb-container danger">
     </div>
     <p class="text">Some warning</p>
 </div>'''
-expected_output = '\\clovisDefinition{Some word}{\n    Some definition\n}\n\n'
+expected_output = '\\clovisDanger{Some warning\\\\\n\n}\n\n'
+result = clovis_to_tex(test_input)
+
+assert result == expected_output
+
+
+# test for katex-code
+test_input = '''<div class="katex-container"><p class="katex-code">5x+3</p></div>'''
+expected_output = '5x+3\\\\\n\n'
+result = clovis_to_tex(test_input)
+
+assert result == expected_output
+
+
+# test for katex-inline-code
+test_input = '<div class="katex-container"><p class="katex-inline-code">Some equation : $5x+3$.<br>A fraction : $ \\dfrac12$, it is a half.</p></div>'
+expected_output = 'Some equation : $5x+3$.\\\\A fraction : $ \\dfrac12$, it is a half.\\\\\n\n'
 result = clovis_to_tex(test_input)
 
 assert result == expected_output
