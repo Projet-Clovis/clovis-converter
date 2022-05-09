d96da101d34101ed3c6606dd31d42a60ae6007bd
Ab2nour <briochekine@hotmail.fr>
2022-05-09 23:22:07 +0200

    Update common.py

--- src/common.py
+++ src/common.py
@@ -1,9 +1,8 @@
-## Functions
 def remove_tags(soup, selector):
     tags = soup.select(selector)
 
     for t in tags:
-        t.decompose() # remove
+        t.decompose()  # remove
 
 
 def rename_tags(soup, selector, new_name='article'):
