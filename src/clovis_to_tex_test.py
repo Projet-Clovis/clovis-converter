0b49790442d14992302602e1649d43850c67eb61
Ab2nour <briochekine@hotmail.fr>
2022-05-09 23:17:05 +0200

    Update clovis_to_tex_test.py

--- src/clovis_to_tex_test.py
+++ src/clovis_to_tex_test.py
@@ -3,7 +3,6 @@ def test(test_input, expected_output):
     assert result == expected_output
 
 
-# test for h1
 def h1_test():
     test_input = '<h1 class="title">Some h1 title</h1>'
     expected_output = '\\section{Some h1 title}\n\n'
@@ -11,7 +10,6 @@ def h1_test():
     test(test_input, expected_output)
 
 
-# test for definition
 def definition_test():
     test_input = '''<div class="cb-container definition"><div class="cb-title-container"><span class="cb-title-icon"></span><span class="cb-title"></span></div><p class="definition-title">Some word</p><p class="text">Some definition</p></div>'''
     expected_output = '\\clovisDefinition{Some word}{\n    Some definition\n}\n\n'
@@ -19,15 +17,14 @@ def definition_test():
     test(test_input, expected_output)
 
 
-# test for colorful-block danger
 def colorful_block_test():
+    # test for colorful-block danger
     test_input = '''<div class="cb-container danger"><div class="cb-title-container"><span class="cb-title-icon"></span><span class="cb-title"></span></div><p class="text">Some warning</p></div>'''
     expected_output = '\\clovisDanger{Some warning}\n\n'
     
     test(test_input, expected_output)
 
 
-# test for katex-code
 def katex_code_test():
     test_input = '''<div class="katex-container"><p class="katex-code">5x+3</p></div>'''
     expected_output = '5x+3\\\\\n\n'
@@ -35,7 +32,6 @@ def katex_code_test():
     test(test_input, expected_output)
 
 
-# test for katex-inline-code
 def katex_inline_code_test():
     test_input = '<div class="katex-container"><p class="katex-inline-code">Some equation : $5x+3$.<br>A fraction : $ \\dfrac12$, it is a half.</p></div>'
     expected_output = 'Some equation : $5x+3$.\\\\A fraction : $ \\dfrac12$, it is a half.\\\\\n\n'
