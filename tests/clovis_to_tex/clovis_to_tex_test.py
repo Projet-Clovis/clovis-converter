04222dbd155000bbe54fae7eb7837cb00f0f9edd
Ab2nour <briochekine@hotmail.fr>
2022-06-10 00:08:44 +0200

    Create clovis_to_tex_test.py

---
+++ tests/clovis_to_tex/clovis_to_tex_test.py
@@ -0,0 +1,49 @@
+from src.clovis_to_tex import clovis_to_tex
+
+
+def test(input_location: str, output_location: str) -> None:
+    with open(input_location) as file:
+        content: str = file.read()
+
+    with open(output_location) as file:
+        expected_output: str = file.read()
+
+    result: str = clovis_to_tex(content)
+
+    assert result == expected_output
+
+
+def h1_test() -> None:
+    test(
+        "../assets/study-sheet/block/core/h1.html",
+        "../assets/study-sheet/block/core/h1.tex",
+    )
+
+
+def definition_test() -> None:
+    test(
+        "../assets/study-sheet/block/core/definition.html",
+        "../assets/study-sheet/block/core/definition.tex",
+    )
+
+
+def colorful_block_test() -> None:
+    # test for colorful-block danger
+    test(
+        "../assets/study-sheet/block/core/colorful-block.html",
+        "../assets/study-sheet/block/core/colorful-block.tex",
+    )
+
+
+def katex_code_test() -> None:
+    test(
+        "../assets/study-sheet/block/core/katex-code.html",
+        "../assets/study-sheet/block/core/katex-code.tex",
+    )
+
+
+def katex_inline_code_test() -> None:
+    test(
+        "../assets/study-sheet/block/core/katex-inline-code.html",
+        "../assets/study-sheet/block/core/katex-inline-code.tex",
+    )
