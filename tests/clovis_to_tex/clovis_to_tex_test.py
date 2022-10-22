60e9f34fafc9153dfabdde746af052958f78fe85
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2022-10-22 17:24:05 +0200

    tests: import

--- tests/clovis_to_tex/clovis_to_tex_test.py
+++ tests/clovis_to_tex/clovis_to_tex_test.py
@@ -1,9 +1,10 @@
 # How to:
+# remove the 1st line import
 # c/c common.py here
 # c/c clovis_to_tex.py here
 # in clovis_to_tex.py, remove the line importing common.py
 # it works ! :)
-from src.clovis_to_tex import clovis_to_tex
+from clovis_to_tex import clovis_to_tex
 
 
 def _check_conversion(input_location: str, output_location: str) -> None:
