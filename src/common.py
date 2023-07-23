1b6ccb447ad7040ccc0708165a56fb5431c3c08c
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-07-23 19:34:27 +0200

    docs: add docs for `rename_tags` function

--- src/common.py
+++ src/common.py
@@ -9,6 +9,13 @@ def remove_tags(soup: BeautifulSoup, selector: str) -> None:
 
 
 def rename_tags(soup: BeautifulSoup, selector: str, new_name: str = "article") -> None:
+    """Rename the tag selected in selector.
+
+    Example:
+        If we have the tag `<p class="test">ok</p>`
+        Running `rename_tags(soup, ".test", "new_p")`
+        will turn the tag to `<new_p class="test">ok</new_p>`
+    """
     tags: ResultSet[Tag] = soup.select(selector)
 
     for t in tags:
